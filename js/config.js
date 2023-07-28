1d24ac2d6d419bba3ec2682ab812cfe122fffa12
palmersola <palmersola@yahoo.com>
2023-07-28 13:38:14 -0500

    updated config with cognito

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
   cognito: {
     userPoolId: "us-east-2_LCJ8EO96v", // e.g. us-east-2_uXboG5pAb
-    userPoolClientId: "25ddkmj4v6hfsfvruhpfi7n4hv", // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-    region: "us-west-2" // e.g. us-east-2
+    userPoolClientId: "13nmr8bsqijm21bha8k48jisr9", // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+    region: "us-east-2" // e.g. us-east-2
   },
   api: {
     invokeUrl: "" // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
